ce5f2e00f0acd24f1b318aa3579bb895f345b2b4
Trinh V. Nguyen <nguyenvantrinh2607@gmail.com>
2022-11-20 21:26:59 +0700

    remove api-handler.sh

--- cryptobash.sh
+++ cryptobash.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 # Enable the execution permission for components
-chmod +x './components/api-handler.sh'
 chmod +x './components/help.sh'
 
-# Get the COINMARKETCAP API key
-./components/api-handler.sh
+# Define the COINMARKETCAP API key
+COINMARKETCAP_API_KEY=12e76001-8ae7-4e49-8d38-40b20bbe6846
 
-#
+# Methods
+# Pass the constants along with the command if needed. 
+# For example: 
+# ./components/search.sh $COINMARKETCAP_API_KEY
 ./components/help.sh
+
 
