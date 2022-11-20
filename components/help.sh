56252cd68dc8a1f12452c13604f486e5fb0c3dfa
Trinh V. Nguyen <nguyenvantrinh2607@gmail.com>
2022-11-20 20:54:56 +0700

    add help.sh and api-handler.sh

---
+++ components/help.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cli=cryptobash
+
+help_text () {
+	while read -r line; do
+		printf "%s\n" "$line"
+	done <<-EOF
+	
+	Usage:
+	  	$cli list [OPTIONS]		list a number of cryptocurrencies (Default: 10)
+	  	$cli search [OPTIONS]		search for a specific cryptocurrencies
+	  	$cli graph [OPTIONS]		display a graph using received data
+	  	$cli predict [OPTIONS]		make a prediction based on current data
+	  	$cli clear			delete all data in data folder
+	  	$cli update			fetch update from github
+	
+	Options:
+	  	-v print version number
+	  	-h show helptext
+
+EOF
+}
+help_text
--- cryptobash.sh
+++ cryptobash.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-if [$COINMARKETCAP_API_KEY == ""]; then
-  command python3 -m webbrowser -t https://pro.coinmarketcap.com/account
-  sleep 5
-  echo "Go to COINMARKETCAP website to get the API key!"
-fi
-while [$COINMARKETCAP_API_KEY == ""]; do 
-  echo "Input your API key below: " 
-  read COINMARKETCAP_API_KEY
-  echo $COINMARKETCAP_API_KEY
-done
-read INPUT
-# curl -H "X-CMC_PRO_API_KEY: $COINMARKETCAP_API_KEY" -H "Accept: application/json" -d "start=1&limit=9&convert=USD" -G https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest
-curl -H "X-CMC_PRO_API_KEY: $COINMARKETCAP_API_KEY" -H "Accept: application/json" -d "convert=USD&slug=$INPUT" -G https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest
+# Enable the execution permission for components
+chmod +x './components/api-handler.sh'
+chmod +x './components/help.sh'
+
+# Get the COINMARKETCAP API key
+./components/api-handler.sh
+
+#
+./components/help.sh
+
