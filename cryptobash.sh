471213fed7f20b9735148958b4763a577e5c7670
giangnt01 <giangnt01@thanglonginc.com>
2022-12-07 19:22:40 +0700

    list

--- cryptobash.sh
+++ cryptobash.sh
@@ -2,6 +2,7 @@
 
 # Enable the execution permission for components
 chmod +x './components/help.sh'
+chmod +x './components/list.sh'
 
 # Define the COINMARKETCAP API key
 COINMARKETCAP_API_KEY=12e76001-8ae7-4e49-8d38-40b20bbe6846
@@ -11,5 +12,6 @@ COINMARKETCAP_API_KEY=12e76001-8ae7-4e49-8d38-40b20bbe6846
 # For example: 
 # ./components/search.sh $COINMARKETCAP_API_KEY
 ./components/help.sh
+./components/list.sh
 
 
